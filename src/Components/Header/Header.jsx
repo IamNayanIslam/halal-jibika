26a7167f580215d170d52158e6ca662688d46e9d
IamNayanIslam <iamnayanislam@gmail.com>
2023-12-31 07:19:42 +0600

    Responsive Navbar Complete

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,28 @@
 import { Link, NavLink } from "react-router-dom";
 import { Navlinks } from "../../Routes/Navlinks";
 import "./Header.css";
+import { FaBars } from "react-icons/fa";
+import { IoCloseSharp } from "react-icons/io5";
+import { useState } from "react";
 
 function Header() {
+  const [mobileNav, setMobileNav] = useState(false);
+
+  const toggleMobileNav = () => {
+    setMobileNav((prevNav) => !prevNav);
+  };
   return (
     <>
       <nav>
-        <Link to="/">
-          <img src="/img/hj.jpg" alt="Halal_jibika" className="logo" />
-        </Link>
-        <ul className="main-menu">
+        <div className="hj">
+          <Link to="/">
+            <img src="/img/hj.jpg" alt="Halal_jibika" className="logo" />
+          </Link>
+          <h2>
+            Halal <span>Jibika</span>
+          </h2>
+        </div>
+        <ul className={`main-menu mobile-ul ${mobileNav && "slide-in"}`}>
           {Navlinks.map((link) => (
             <li key={link.name}>
               <NavLink to={link.path}>{link.name}</NavLink>
@@ -25,6 +38,13 @@ function Header() {
               <Link to="/signup">Sign up</Link>
             </li>
           </ul>
+          <div className="open-close">
+            {mobileNav ? (
+              <IoCloseSharp onClick={toggleMobileNav} />
+            ) : (
+              <FaBars onClick={toggleMobileNav} />
+            )}
+          </div>
         </div>
       </nav>
     </>
